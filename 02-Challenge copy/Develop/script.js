8ba2533eeb11e4bef2a4c853023bc73036a4fd8a
NotADullJack <135572463+NotADullJack@users.noreply.github.com>
2023-07-06 22:42:17 -0500

    Add files via upload
    added popout alerts that prompt you with questions and then generates your password

--- 02-Challenge copy/Develop/script.js
+++ 02-Challenge copy/Develop/script.js
@@ -1,7 +1,7 @@
-// Assignment Code
+
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -10,5 +10,35 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
+
 generateBtn.addEventListener("click", writePassword);
+
+// this function 
+function generatePassword(length, includeUppercase, includeNumbers, includeSpecialChars) {
+  var charset = "abcdefghijklmnopqrstuvwxyz";
+  var password = "";
+if (includeUppercase) {
+    charset += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  }
+if (includeNumbers) {
+    charset += "0123456789";
+  }
+if (includeSpecialChars) {
+    charset += "!@#$%^&*()";
+  }
+for (var i = 0; i < length; i++) {
+    password += charset.charAt(Math.floor(Math.random() * charset.length));
+  }
+return password;
+}
+
+// asks if you want uppercase letters
+var uppercaseResponse = confirm("Would you like uppercase letters in your password?");
+// asks if you want numbers 
+var numbersResponse = confirm("Would you like numbers in your password?");
+// asks if you want characters 
+var specialCharsResponse = confirm("Would you like special characters in your password?");
+// generates the ppassword
+var password = generatePassword(10, uppercaseResponse, numbersResponse, specialCharsResponse);
+// Show the generated password in an alert box
+alert("Your generated password is: " + password);
